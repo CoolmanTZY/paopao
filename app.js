558996c565c60401d0bdf51e9113112a75e9a18c
田振毅 <ttzy0202@gmail.com>
2019-08-17 09:30:15 +0800

    3.首页完成

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 //app.js
+import request from "./utils/request.js";
 App({
+  
   onLaunch: function () {
+    // 初始化request 的基准路径
+    request.defaults.baseURL = "https://api.zbztb.cn/api/public/v1";
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,41 @@
+import request from "../../utils/request.js";
 Page({
-  data:{},
-  onLoad:function(){
-    console.log(123)
-  }
+  data: {
+    // 轮播图数据
+    imgUrls: [],
+    // 导航栏数据
+    catitems:[],
+    // 楼层数据
+    floordata:[]
+  },
+  onLoad() {
+    request({
+      url:"/home/swiperdata"
+    }).then(res => {
+      const { message } = res.data
+      this.setData({
+        imgUrls: message
+      })
+    }),
+    
+    request({
+      url:'/home/catitems',
+    }).then(res=>{
+      const {message} = res.data
+      this.setData({
+        catitems:message
+      })
+    }),
+
+    // 楼层数据
+    request({
+      url:'/home/floordata'
+    }).then(res=>{
+      const {message} = res.data
+      this.setData({
+        floordata:message
+      })
+    })
+  },
+
 })
