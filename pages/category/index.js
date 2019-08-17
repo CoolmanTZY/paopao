7767b605db99ad38004e5c6425e4839db3619b7d
田振毅 <ttzy0202@gmail.com>
2019-08-17 19:24:17 +0800

    4.分类页完成(导航栏拆分)

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,36 @@
-// pages/category/index.js
+import request from '../../utils/request.js'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    // 左侧导航栏的数据
+    categoriesList:[],
+    // 导航栏样式的添加
+    active:0,
+    // 右侧内容
+  },
+  // 点击左侧时切换样式
+  handleClick(e){
+    const { index } = e.currentTarget.dataset;
+    this.setData({
+      active:index
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    request({
+      url: "/categories"
+    }).then(res=>{
+      const { message } = res.data
+      this.setData({
+        categoriesList:message
+      })
+      console.log(this.data.categoriesList)
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
